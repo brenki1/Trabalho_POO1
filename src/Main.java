930f3d29e296e3560e36e88d41749d0d1225f57d
ViniciusLimaCarvalho <viniciuscarvalho@ufu.br>
2024-03-07 00:00:58 -0300

    Classe VerificaData pronta

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,6 @@ import java.time.*;
 
 public class Main{
     public static void main(String[] args) {
-        //Pessoa p1 = new Pessoa("joao", "11/11/2005","111222333-99","Avenida Joao naves de avila");
-
-        //SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-
-        //System.out.println("Nome: " + p1.getNome() + "\nData de nascimento: " + p1.getDataNascimento() + " CPF: " + p1.getCPF() + " Endereco: " + p1.getEndereco());
-        //System.out.println(formato.format(p1.getdNas()));
-
 
 
     }
--- src/VerificaData.java
+++ src/VerificaData.java
@@ -14,8 +14,81 @@ public class VerificaData {
 
     }
 
-    public static LocalDateTime dataHoraCorreta(String dataHora){
+    public static LocalDate dataCorreta(String data){
+        String anoStr = data.substring(0,4);
+        String mesStr = data.substring(5,7);
+        String diaStr = data.substring(8,10);
+
+        int anoInt = Integer.parseInt(anoStr);
+        int mesInt = Integer.parseInt(mesStr);
+        int diaInt = Integer.parseInt(diaStr);
+
+        return dataCorreta(anoInt,mesInt,diaInt);
+    }
+
+    public static LocalDateTime dataHoraCorreta(int ano, int mes, int dia, int hora, int minuto, int segundo){
+        if(verificaAno(ano) && verificaMes(mes) && verificaDia(ano,mes,dia) && verificaHora(hora) && verificaMinuto(minuto) && verificaSegundo(segundo)) {
+            return LocalDateTime.of(ano, mes, dia, hora, minuto, segundo);
+        }
+
         return LocalDateTime.now();
+    }
+    public static LocalDateTime dataHoraCorreta(String dataHora){
+        String anoStr = dataHora.substring(0,4);
+        String mesStr = dataHora.substring(5,7);
+        String diaStr = dataHora.substring(8,10);
+        String horaStr = dataHora.substring(11,13);
+        String minutoStr = dataHora.substring(14,16);
+        String segundoStr = dataHora.substring(17,19);
+
+        int anoInt = Integer.parseInt(anoStr);
+        int mesInt = Integer.parseInt(mesStr);
+        int diaInt = Integer.parseInt(diaStr);
+        int horaInt = Integer.parseInt(horaStr);
+        int minutoInt = Integer.parseInt(minutoStr);
+        int segundoInt = Integer.parseInt(segundoStr);
+
+        return dataHoraCorreta(anoInt,mesInt,diaInt,horaInt,minutoInt,segundoInt);
+    }
+
+    public static boolean verificaData(String data){
+        String anoStr = data.substring(0,4);
+        String mesStr = data.substring(5,7);
+        String diaStr = data.substring(8,10);
+
+        int anoInt = Integer.parseInt(anoStr);
+        int mesInt = Integer.parseInt(mesStr);
+        int diaInt = Integer.parseInt(diaStr);
+
+        if(verificaAno(anoInt) && verificaMes(mesInt)){
+            if(verificaDia(anoInt,mesInt,diaInt)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static boolean verificaDataHora(String dataHora){
+        String anoStr = dataHora.substring(0,4);
+        String mesStr = dataHora.substring(5,7);
+        String diaStr = dataHora.substring(8,10);
+        String horaStr = dataHora.substring(11,13);
+        String minutoStr = dataHora.substring(14,16);
+        String segundoStr = dataHora.substring(17,19);
+
+        int anoInt = Integer.parseInt(anoStr);
+        int mesInt = Integer.parseInt(mesStr);
+        int diaInt = Integer.parseInt(diaStr);
+        int horaInt = Integer.parseInt(horaStr);
+        int minutoInt = Integer.parseInt(minutoStr);
+        int segundoInt = Integer.parseInt(segundoStr);
+
+        if(verificaAno(anoInt) && verificaMes(mesInt) && verificaDia(anoInt,mesInt,diaInt) && verificaHora(horaInt) && verificaMinuto(minutoInt) && verificaSegundo(segundoInt)) {
+            return true;
+        }
+
+        return false;
     }
 
     private static boolean verificaAno(int ano){
@@ -47,8 +120,24 @@ public class VerificaData {
                 if(dia == 29 && !LocalDate.of(ano,mes,1).isLeapYear()){
                     return false;
                 }
-                else return dia>=1 && dia <= 28;
+                else if(dia == 29 && LocalDate.of(ano,mes,1).isLeapYear()){
+                    return true;
+                }
+                else{
+                    return dia >=1 && dia <= 28;
+                }
         }
         return false;
+    }
+
+    private static boolean verificaHora(int hora){
+        return hora >=0 && hora < 24;
+    }
+
+    private static boolean verificaMinuto(int minuto){
+        return minuto >=0 && minuto < 60;
+    }
+    private static boolean verificaSegundo(int segundo){
+        return segundo >=0 && segundo < 60;
     }
 }
