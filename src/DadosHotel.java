f694f899a6f9045820cba80f8541fea0024845f2
GustavoMarques345 <Gustavo.oliveira6@ufu.br>
2024-04-07 21:33:30 -0300

    Armazena dados do Hoteal

---
+++ src/DadosHotel.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class DadosHotel {
+    //Array que armazena os dados do Hotel
+    private ArrayList<Hotel>hoteis;
+
+    public DadosHotel() {
+        this.hoteis = new ArrayList<>();
+    }
+
+    // Método para cadastrar um  hotel
+    public void cadastrar(Hotel hotel) {
+        hoteis.add(hotel);
+    }
+
+    // Método para excluir um hotel
+    public boolean excluir(Hotel hotel) {
+        return hoteis.remove(hotel);
+    }
+
+    // Método para listar todos os hotéis
+    public void listarHoteis() {
+        for (Hotel hotel : hoteis) {
+            System.out.println(hotel);
+        }
+    }
+
+}
