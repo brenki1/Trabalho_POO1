49b2c0de8853c895836f0903dbd9e8d3c8394f4b
GustavoMarques345 <Gustavo.oliveira6@ufu.br>
2024-04-07 21:23:43 -0300

    Classe para armazenar os dados da Compra

---
+++ src/DadosCompra.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class DadosCompra {
+    //Array para armazenar os dados da compras
+    private ArrayList<Compra>compras;
+
+    public DadosCompra() {
+        this.compras = new ArrayList<>(); //Inicializando um Vetor
+    }
+
+    // Método para cadastrar uma  compra
+    public void cadastrar(Compra compra) {
+        compras.add(compra);
+    }
+
+    // Método para buscar uma compra pelo comprador
+    public Compra buscar(Cliente comprador) {
+        for (Compra compra : compras) {
+            if (compra.getComprador().equals(comprador)) {
+                return compra;
+            }
+        }
+        return null; // Compra não encontrada
+    }
+
+    // Método para excluir uma compra
+    public boolean excluirCompra(Compra compra) {
+        return compras.remove(compra);
+    }
+
+
+
+    // Método para listar todas as compras
+    public void listar() {
+        for (Compra compra : compras) {
+            System.out.println(compra); // Supondo que Compra tenha um método toString() implementado
+        }
+    }
+}
+
+
