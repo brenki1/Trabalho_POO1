3ef5f0040478b36e98ae033b1aba99e453c31cc5
brenki1 <pagioliosvaldo@gmail.com>
2024-04-11 20:08:47 -0300

    palavras não são o suficientes para descrever essa Arte.

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -3,30 +3,21 @@ package Classes;
 import GUI.*;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 import java.util.Scanner;
 
 public class Main extends JFrame {
+
     public static void main(String[] args) {
+
         MenuPrincipal Menu = new MenuPrincipal();
         Menu.setContentPane(Menu.MenuPrincipal);
         Menu.setVisible(true);
         Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Menu.setSize(638,500);
+        Menu.setSize(638, 500);
         Menu.setTitle("Udi-Decola");
 
-        MenuClienteLogin LoginC = new MenuClienteLogin();
-        LoginC.setContentPane(LoginC.ClienteLogin);
-        LoginC.setVisible(true);
-        LoginC.setSize(638,500);
-        LoginC.setTitle("Udi-Decola");
-        LoginC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        MenuAdmin MenuADM = new MenuAdmin();
-        MenuADM.setContentPane(MenuADM.MenuAdmins);
-        MenuADM.setVisible(true);
-        MenuADM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        MenuADM.setSize(620,350);
-        MenuADM.setTitle("Udi-Decola ADMIN");
     }
-
 }
--- src/GUI/MenuPrincipal.java
+++ src/GUI/MenuPrincipal.java
@@ -6,17 +6,28 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 
-public class MenuPrincipal extends JFrame   {
+public class MenuPrincipal extends JFrame {
 
     public MenuPrincipal() {
-        initComponents();
+
+        initComponentsMenuPrinc();
+        initComponentsMenuLogin();
+        initComponentsLogarCliente();
+        initComponentsCADCliente();
+        TrataBotoes tratador = new TrataBotoes();
+        butCliente.addActionListener(tratador);
+        Cadastrar.addActionListener(tratador);
+        Logar.addActionListener(tratador);
+
     }
+
+
 
     private void butClienteMouseClicked(MouseEvent e) {
         // TODO add your code here
     }
 
-    private void initComponents() {
+    private void initComponentsMenuPrinc() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         MenuPrincipal = new JPanel();
         msgBemvindoPrinc = new JLabel();
@@ -25,6 +36,7 @@ public class MenuPrincipal extends JFrame   {
         msgMenuAcess = new JLabel();
         butCliente = new JButton();
         butAdm = new JButton();
+
 
         //======== MenuPrincipal ========
         {
@@ -64,12 +76,6 @@ public class MenuPrincipal extends JFrame   {
 
             //---- butCliente ----
             butCliente.setText("Cliente");
-            butCliente.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    butClienteMouseClicked(e);
-                }
-            });
             MenuPrincipal.add(butCliente, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 new Insets(0, 0, 33, 16), 0, 0));
@@ -80,7 +86,195 @@ public class MenuPrincipal extends JFrame   {
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                 new Insets(0, 0, 33, 16), 0, 0));
         }
+
+
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    public void initComponentsMenuLogin () {
+
+        ClienteLogin = new JPanel();
+        Pergunta = new JLabel();
+        Logar = new JButton();
+        Pergunta2 = new JLabel();
+        Cadastrar = new JButton();
+
+        ///======== ClienteLogin ========
+        {
+            ClienteLogin.setLayout(new GridBagLayout());
+
+            //---- Pergunta ----
+            Pergunta.setText("J\u00e1 possui uma conta?");
+            Pergunta.setFont(Pergunta.getFont().deriveFont(Pergunta.getFont().getSize() + 3f));
+            Pergunta.setHorizontalAlignment(SwingConstants.LEFT);
+            ClienteLogin.add(Pergunta, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 20, 5), 0, 0));
+
+            //---- Logar ----
+            Logar.setText("Fazer Login");
+            ClienteLogin.add(Logar, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 20, 5), 0, 0));
+
+            //---- Pergunta2 ----
+            Pergunta2.setText("N\u00e3o possui uma conta?");
+            Pergunta2.setFont(Pergunta2.getFont().deriveFont(Pergunta2.getFont().getSize() + 3f));
+            ClienteLogin.add(Pergunta2, new GridBagConstraints(2, 5, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 20, 5), 0, 0));
+
+            //---- Cadastrar ----
+            Cadastrar.setText("Cadastro");
+            ClienteLogin.add(Cadastrar, new GridBagConstraints(2, 7, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 5), 0, 0));
+        }
+    }
+
+    public void initComponentsLogarCliente () {
+
+        LoginCliente = new JPanel();
+        LogCPF = new JLabel();
+        CPFLogInsert = new JTextField();
+        LogEmail = new JLabel();
+        EmailLogInsert = new JTextField();
+        botaoLogin = new JPanel();
+        okButton2 = new JButton();
+        cancelButton2 = new JButton();
+
+        //======== LoginCliente ========
+        {
+            LoginCliente.setLayout(new GridBagLayout());
+            ((GridBagLayout)LoginCliente.getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+            ((GridBagLayout)LoginCliente.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+
+            //---- LogCPF ----
+            LogCPF.setText("CPF:");
+            LoginCliente.add(LogCPF, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+            LoginCliente.add(CPFLogInsert, new GridBagConstraints(1, 2, 6, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+
+            //---- LogEmail ----
+            LogEmail.setText("Email:");
+            LoginCliente.add(LogEmail, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+            LoginCliente.add(EmailLogInsert, new GridBagConstraints(1, 4, 6, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+
+            //======== botaoLogin ========
+            {
+                botaoLogin.setBorder(new EmptyBorder(12, 0, 0, 0));
+                botaoLogin.setLayout(new GridBagLayout());
+                ((GridBagLayout)botaoLogin.getLayout()).columnWidths = new int[] {0, 85, 80};
+                ((GridBagLayout)botaoLogin.getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0};
+
+                //---- okButton2 ----
+                okButton2.setText("OK");
+                botaoLogin.add(okButton2, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- cancelButton2 ----
+                cancelButton2.setText("Cancel");
+                botaoLogin.add(cancelButton2, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 0), 0, 0));
+            }
+            LoginCliente.add(botaoLogin, new GridBagConstraints(6, 6, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.EAST, GridBagConstraints.VERTICAL,
+                    new Insets(0, 0, 0, 0), 0, 0));
+        }
+
+    }
+
+    public void initComponentsCADCliente() {
+
+        CadastroCliente = new JPanel();
+        label1 = new JLabel();
+        textField1 = new JTextField();
+        label4 = new JLabel();
+        textField2 = new JTextField();
+        label5 = new JLabel();
+        textField3 = new JTextField();
+        label6 = new JLabel();
+        textField4 = new JTextField();
+        botaoCad = new JPanel();
+        okButton = new JButton();
+        cancelButton = new JButton();
+
+
+        //======== CadastroCliente ========
+        {
+            CadastroCliente.setLayout(new GridBagLayout());
+            ((GridBagLayout)CadastroCliente.getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0};
+            ((GridBagLayout)CadastroCliente.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+
+            //---- label1 ----
+            label1.setText("Nome:");
+            CadastroCliente.add(label1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+            CadastroCliente.add(textField1, new GridBagConstraints(1, 1, 5, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+
+            //---- label4 ----
+            label4.setText("CPF:");
+            CadastroCliente.add(label4, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+            CadastroCliente.add(textField2, new GridBagConstraints(1, 2, 5, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+
+            //---- label5 ----
+            label5.setText("Endere\u00e7o:");
+            CadastroCliente.add(label5, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+            CadastroCliente.add(textField3, new GridBagConstraints(1, 3, 5, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+
+            //---- label6 ----
+            label6.setText("Email:");
+            CadastroCliente.add(label6, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 5), 0, 0));
+            CadastroCliente.add(textField4, new GridBagConstraints(1, 4, 5, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 5, 0), 0, 0));
+
+            //======== botaoCad ========
+            {
+                botaoCad.setBorder(new EmptyBorder(12, 0, 0, 0));
+                botaoCad.setLayout(new GridBagLayout());
+                ((GridBagLayout)botaoCad.getLayout()).columnWidths = new int[] {0, 85, 80};
+                ((GridBagLayout)botaoCad.getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0};
+
+                //---- okButton ----
+                okButton.setText("OK");
+                botaoCad.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- cancelButton ----
+                cancelButton.setText("Cancel");
+                botaoCad.add(cancelButton, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 0), 0, 0));
+            }
+            CadastroCliente.add(botaoCad, new GridBagConstraints(5, 5, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.EAST, GridBagConstraints.VERTICAL,
+                    new Insets(0, 0, 0, 0), 0, 0));
+        }
+
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
@@ -89,8 +283,55 @@ public class MenuPrincipal extends JFrame   {
     private JLabel label2;
     private JLabel label3;
     private JLabel msgMenuAcess;
-    private JButton butCliente;
-    private JButton butAdm;
+    public JButton butCliente;
+    public JButton butAdm;
+    public JPanel ClienteLogin;
+    private JLabel Pergunta;
+    private JButton Logar;
+    private JLabel Pergunta2;
+    private JButton Cadastrar;
+    public JPanel CadastroCliente;
+    private JLabel label1;
+    private JTextField textField1;
+    private JLabel label4;
+    private JTextField textField2;
+    private JLabel label5;
+    private JTextField textField3;
+    private JLabel label6;
+    private JTextField textField4;
+    private JPanel botaoCad;
+    private JButton okButton;
+    private JButton cancelButton;
+    public JPanel LoginCliente;
+    private JLabel LogCPF;
+    private JTextField CPFLogInsert;
+    private JLabel LogEmail;
+    private JTextField EmailLogInsert;
+    private JPanel botaoLogin;
+    private JButton okButton2;
+    private JButton cancelButton2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+
+    private class TrataBotoes implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == butCliente) {
+                MenuPrincipal.setVisible(false);
+                ClienteLogin.setVisible(true);
+                setContentPane(ClienteLogin);
+            }
+
+            if(e.getSource() == Cadastrar) {
+                ClienteLogin.setVisible(false);
+                CadastroCliente.setVisible(true);
+                setContentPane(CadastroCliente);
+            }
+
+            if(e.getSource() == Logar) {
+                ClienteLogin.setVisible(false);
+                LoginCliente.setVisible(true);
+                setContentPane(LoginCliente);
+            }
+        }
+    }
 
 }
